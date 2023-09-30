ff084fd56cba76003b6cedaae81e12697f5afb74
Molyamqzy <narmahanmoldir@gmail.com>
2023-09-30 14:07:49 +0600

    first commit

---
+++ dart_lesson_2.dart
@@ -0,0 +1,47 @@
+// void main(){
+//     print('hello');
+// }
+// void aboutMyself{
+//     print('Hi, my name is Moldir, i am from Taraz')
+// }
+// /* comment*/
+
+// void main(){
+//     int x = 2;
+//     print(((7*x)*2)-((3*x)+6)); 
+// }
+
+// void main(){
+//      int a = 2;
+//     print(12*a*2 +7*a -16);
+// }
+  
+  //  import 'dart:math';
+
+// void  main(){
+//     int a = 2;
+//     print((pow(a,2)+10)/(sqrt(pow(a,2))+1));
+//    }
+
+//     import 'dart:math';
+
+// void main(){
+//       int a = 2;
+//       print(sqrt(2*a+sin(3*a))/3.56);
+//     }
+
+    // void main(){
+    //   int a = 1234;
+    //   print(a/10);
+    //   print(a%10);
+    //   print(a%10);
+    //   print((a%100)/10);
+    //   print((a%1000)/100);
+    //   print(a/1000);
+    //   print((a%10)+((a%100)/10)+((a%1000)/100)+(a/1000));      
+
+    // }
+
+    
+
+
